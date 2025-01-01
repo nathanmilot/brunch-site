fadd85b34ef9d2432d3f19a0101c53d58d3ba964
nathanmilot <nathanmilot@users.noreply.github.com>
2025-01-01 10:05:09 +0000

    deploy: 770492e2d229cf5038bf11f2996ca7b9435e14d6

--- script.js
+++ script.js
@@ -6,6 +6,35 @@ function titleCase(str) {
   }).join(' ');
 }
 
+// Check the system theme preference on page load
+const systemPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// Set initial theme based on system preference
+const icon = document.getElementById("toggle-icon");
+if (systemPrefersDark) {
+  document.body.classList.add('light-mode');
+  icon.classList.add('fa-moon');
+} else {
+  document.body.classList.remove('light-mode');
+  icon.classList.add('fa-sun');
+}
+
+// Function to toggle the mode
+const toggleMode = () => {
+  const body = document.body;
+  body.classList.toggle('light-mode');
+  
+  // Toggle between sun and moon icons
+  const icon = document.getElementById("toggle-icon");
+  if (body.classList.contains('light-mode')) {
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+  } else {
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  }
+};
+
 
 // Path to the dynamically created secrets file
 const secretsPath = './secrets.json';
@@ -37,6 +66,7 @@ fetch(secretsPath)
         data.events.forEach(event => {
           const { details, description, menu } = event;
           const li = document.createElement('li');
+          li.classList.add("raised");
           li.innerHTML = `
         <strong>${details.date}</strong>: ${details.title} 
         <br>Location: ${details?.location ?? defaultLocation}
