b39a5dfdd06e2bc17ca298685b40fa9c6b5571b9
nathanmilot <nathanmilot@users.noreply.github.com>
2025-01-01 11:10:04 +0000

    deploy: e6d1a034a8bc6a255f4d3d9c0e4fa761333c345b

--- script.js
+++ script.js
@@ -1,55 +1,65 @@
 var defaultLocation = "";
 
 function titleCase(str) {
-  return str.toLowerCase().split(' ').map(function (word) {
-    return (word.charAt(0).toUpperCase() + word.slice(1));
-  }).join(' ');
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
 }
 
-// Check the system theme preference on page load
-const systemPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+const systemPrefersDark = () =>
+  window?.matchMedia?.("(prefers-color-scheme:dark)")?.matches ?? false;
 
 // Set initial theme based on system preference
 const icon = document.getElementById("toggle-icon");
 if (systemPrefersDark) {
-  document.body.classList.add('light-mode');
-  icon.classList.add('fa-moon');
+  document.body.classList.remove("light-mode");
+  icon.classList.add("fa-sun");
 } else {
-  document.body.classList.remove('light-mode');
-  icon.classList.add('fa-sun');
+  document.body.classList.add("light-mode");
+  icon.classList.add("fa-moon");
 }
 
 // Function to toggle the mode
 const toggleMode = () => {
   const body = document.body;
-  body.classList.toggle('light-mode');
-  
+  body.classList.toggle("light-mode");
+  body.class;
+
   // Toggle between sun and moon icons
   const icon = document.getElementById("toggle-icon");
-  if (body.classList.contains('light-mode')) {
-    icon.classList.remove('fa-sun');
-    icon.classList.add('fa-moon');
+  if (body.classList.contains("light-mode")) {
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
   } else {
-    icon.classList.remove('fa-moon');
-    icon.classList.add('fa-sun');
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
   }
 };
 
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    toggleMode();
+  });
 
 // Path to the dynamically created secrets file
-const secretsPath = './secrets.json';
+const secretsPath = "./secrets.json";
 
 // Fetch the secrets JSON file
 fetch(secretsPath)
   .then((response) => {
     if (!response.ok) {
-      throw new Error('Failed to load secrets.json');
+      throw new Error("Failed to load secrets.json");
     }
     return response.json();
   })
   .then((data) => {
     // Update Google Calendar iframe
-    const calendar = document.getElementById('calendar');
+    const calendar = document.getElementById("calendar");
     if (calendar) {
       calendar.src = data.calendarUrl;
     }
@@ -57,27 +67,31 @@ fetch(secretsPath)
     // Update default location display
     defaultLocation = decodeURIComponent(data.defaultLocation);
   })
-  .then( () => {
-    fetch('./data/events.json')
-      .then(response => response.json())
-      .then(data => {
-        const eventsList = document.getElementById('events-list');
+  .then(() => {
+    fetch("./data/events.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const eventsList = document.getElementById("events-list");
 
-        data.events.forEach(event => {
+        data.events.forEach((event) => {
           const { details, description, menu } = event;
-          const li = document.createElement('li');
+          const li = document.createElement("li");
           li.classList.add("raised");
           li.innerHTML = `
         <span><strong>${details.date}</strong>: ${details.title} </span>
         <br><span>Location: ${details?.location ?? defaultLocation}</span>
         ${description}
-        <br><br>Menu: <br>${menu.map(function (item) {return titleCase(item)}).join(", ")}
+        <br><br>Menu: <br>${menu
+          .map(function (item) {
+            return titleCase(item);
+          })
+          .join(", ")}
       `;
           eventsList.appendChild(li);
         });
       })
-      .catch(error => console.error('Error loading JSON:', error));
+      .catch((error) => console.error("Error loading JSON:", error));
   })
   .catch((error) => {
-    console.error('Error loading secrets:', error);
+    console.error("Error loading secrets:", error);
   });
