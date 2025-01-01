6e28af000373467db06a5626a6769d2a75b1f036
nathanmilot <nathanmilot@users.noreply.github.com>
2025-01-01 04:30:39 +0000

    deploy: 3f11722c45e9426a1884cf53b6f3ac871cfee759

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
 var defaultLocation = "";
 
 function titleCase(str) {
-    return str.toLowerCase().split(' ').map(function (word) {
-        return (word.charAt(0).toUpperCase() + word.slice(1));
-    }).join(' ');
+  return str.toLowerCase().split(' ').map(function (word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
 }
 
 
-//document.addEventListener('DOMContentLoaded', () => {
-  // Path to the dynamically created secrets file
-  const secretsPath = './secrets.json';
+// Path to the dynamically created secrets file
+const secretsPath = './secrets.json';
 
-  // Fetch the secrets JSON file
-  fetch(secretsPath)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Failed to load secrets.json');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // Update Google Calendar iframe
-      const calendar = document.getElementById('calendar');
-      if (calendar) {
-        calendar.src = data.calendarUrl;
-      }
+// Fetch the secrets JSON file
+fetch(secretsPath)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Failed to load secrets.json');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Update Google Calendar iframe
+    const calendar = document.getElementById('calendar');
+    if (calendar) {
+      calendar.src = data.calendarUrl;
+    }
 
-      // Update default location display
-      defaultLocation = decodeURIComponent(data.defaultLocation);
-    })
-    .then( () => {
-      fetch('./data/events.json')
-        .then(response => response.json())
-        .then(data => {
-          const eventsList = document.getElementById('events-list');
+    // Update default location display
+    defaultLocation = decodeURIComponent(data.defaultLocation);
+  })
+  .then( () => {
+    fetch('./data/events.json')
+      .then(response => response.json())
+      .then(data => {
+        const eventsList = document.getElementById('events-list');
 
-          data.events.forEach(event => {
-            const { details, description, menu } = event;
-            const li = document.createElement('li');
-            li.innerHTML = `
+        data.events.forEach(event => {
+          const { details, description, menu } = event;
+          const li = document.createElement('li');
+          li.innerHTML = `
         <strong>${details.date}</strong>: ${details.title} 
         <br>Location: ${details?.location ?? defaultLocation}
         <p>${description}</p>
         <p>Menu: <br>${menu.map(function (item) {return titleCase(item)}).join(", ")}</p>
       `;
-            eventsList.appendChild(li);
-          });
-        })
-        .catch(error => console.error('Error loading JSON:', error));
-    })
-    .catch((error) => {
-      console.error('Error loading secrets:', error);
-    });
-//});
+          eventsList.appendChild(li);
+        });
+      })
+      .catch(error => console.error('Error loading JSON:', error));
+  })
+  .catch((error) => {
+    console.error('Error loading secrets:', error);
+  });
