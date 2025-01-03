8e11b3171304c6b07bb5456fb33b80b22de840f9
Nathan Milot <nathan.c.milot@gmail.com>
2025-01-03 16:25:39 -0500

    Minor event card UI improvement

--- script.js
+++ script.js
@@ -4,64 +4,65 @@ function titleCase(str) {
   return str
     .toLowerCase()
     .split(" ")
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 }
 
 function createEventCard(event) {
   const eventDateTime = new Date(event.details.date);
-  const formattedDate = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    timeZone: 'America/New_York'
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    timeZone: "America/New_York",
   }).format(eventDateTime);
 
-  const formattedTime = new Intl.DateTimeFormat('en-US', {
-    hour: 'numeric',
-    minute: 'numeric',
+  const formattedTime = new Intl.DateTimeFormat("en-US", {
+    hour: "numeric",
+    minute: "numeric",
     hour12: true,
-    timeZone: 'America/New_York'
+    timeZone: "America/New_York",
   }).format(eventDateTime);
 
   return `<div class="event-card">
       <div class="event-card__header">
-        <h2 class="event-card__title">${event.details.title}</h2>
+        <div>  
+          <h2 class="event-card__title">${event.details.title}</h2>
+          <div class="event-card__location">
+            <i class="fa-solid fa-location-dot"></i>
+            <div>${event.details?.location ?? defaultLocation}</div>
+          </div>
+        </div>
         <div class="event-card__date">
           <i class="fas fa-calendar-days"></i>
-          <span>
-            <span class="event-card__accent">${formattedDate}</span>
-            <span class="event-card__accent">${formattedTime}</span>
-          </span>
+          <div>
+            <div class="event-card__accent">${formattedDate}</div>
+            <div class="event-card__accent">${formattedTime}</div>
+          </div>
         </div>
-      </div>
-      
-      <div class="event-card__location">
-        <i class="fa-solid fa-location-dot"></i>
-        <span>${event.details?.location ?? defaultLocation}</span>
-      </div>
+      </div>      
 
       <div class="event-card__description">
         ${event.description}
       </div>
 
-      ${event.menu && event.menu.length > 0 && (
-      `<div class="event-card__menu">
+      ${
+        event.menu &&
+        event.menu.length > 0 &&
+        `<div class="event-card__menu">
         <div class="event-card__menu-header">
           <i class="fa-solid fa-utensils"></i>
           <span class="event-card__accent">This Month's Menu</span>
         </div>
         <div class="event-card__menu-items">
-          ${event.menu.map(
-        function (item, index) {
-          return `<span key=${index} class="menu-item">${titleCase(item)}</span>`;
-        }
-      ).join("")
-      }
+          ${event.menu
+            .map(function (item, index) {
+              return `<span key=${index} class="menu-item">${titleCase(item)}</span>`;
+            })
+            .join("")}
         </div >
       </div >`
-    )
-    }
+      }
     </div > `;
 }
 
@@ -116,8 +117,7 @@ fetch(secretsPath)
     // Update Google Calendar iframe
     const calendar = document.getElementById("calendar");
     if (calendar) {
-      const calendarOptions =
-        "&mode=AGENDA&showPrint=0&showCalendars=0";
+      const calendarOptions = "&mode=AGENDA&showPrint=0&showCalendars=0";
       calendar.src = data.calendarUrl + calendarOptions;
     }
 
